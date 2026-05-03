a760420520129d3c50ee950a1153c728e7dae3c3
EnigmaticLantern <279139536+EnigmaticLantern@users.noreply.github.com>
2026-05-02 20:45:27 -0700

    Final Update

--- Final_Project_Hub.cpp
+++ Final_Project_Hub.cpp
@@ -11,32 +11,32 @@ using namespace std;
 int main(){
     /* Prompts the user to input the initial amount */
     cout << "How much did you make in this past quarter?: ";
-    int initial_amount;
-    cin >> initial_amount;
-    double result = initial_amount;
-    employ(result);
-    cout << "There is $" << fixed << setprecision(2) << result << " remaining." << endl;
-    cout << "How much needs to be spent on maintenence/machine repairs/etc?: ";
-    float amount_two;
+    double *result = new double;
+    cin >> *result;
+    employ(result);   
+    cout << "There is $" << fixed << setprecision(2) << *result << " remaining." << endl;
+    cout << "How much needs to be spent on maintenence, machine repairs, etc?: ";
+    double amount_two;
     cin >> amount_two;
-    float result_two;
-    float leftOver;
-    rev_val(result);
-    cout << leftOver << endl;
-    result = leftOver;
-    if (result > 0)
-        cout << "There is $" << result << " remaining." << endl;
-        cout << "You have some left over, would you like to give raises, save it, or do something else with it (other)? (R, S, or O): ";
-        string decision1;
-        cin >> decision1;
-        if (decision1 == "R" || decision1 == "r")
-            int decide(result);
-        if (decision1 == "S" || decision1 == "s")
-            int decide(result);
-        if (decision1 == "O" || decision1 == "o")
+    double result_two;
+    rev_val(result); 
+    if (*result > 0.0) {
+        cout << "There is $" << *result << " remaining." << endl;
+        cout << "You have some left over, would you like to give raises, save it, use it for the pool table, or do something else with it (other)? (R, S, T, or O): ";
+        string decisionOne;
+        cin >> decisionOne;
+        if (decisionOne == "R" || decisionOne == "r")
+            decide(decisionOne, result);
+        if (decisionOne == "S" || decisionOne == "s")
+            decide(decisionOne, result);
+        if (decisionOne == "T" || decisionOne == "t")
+            decide(decisionOne, result);
+        if (decisionOne == "O" || decisionOne == "o")
             cout << "Thank you for using our calculator." << endl << "Have a good day/night!";
-    if (result < 0)
-        cout << "You don't have enought to deliver raises or put into savings";
-        cout << "You are $" << result << " in debt.";
+    }
+    if (*result < 0.0) {
+        cout << "You don't have enought to deliver raises or put into savings" << endl;
+        cout << "You are $" << *result << " in debt.";
+    }
     return 0;
 }
